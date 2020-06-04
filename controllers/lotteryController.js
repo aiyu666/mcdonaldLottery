e1da87109075d36162f16f3a1713730577c18933
aiyu666 <zrobin8741@gmail.com>
2020-06-04 14:24:51 +0800

    Fix get lottery content

--- controllers/lotteryController.js
+++ controllers/lotteryController.js
@@ -4,7 +4,13 @@ const lottery = require('../models/lottery');
 module.exports = async (req, res) => {
   const resp = await lottery.getLottery(req.body.accessToken);
   await res.status(resp.statusCode);
-  await res.json({
-    lottery: resp.body.rm,
-  });
+  if (resp.body.rc !== 1) {
+    await res.status(resp.statusCode);
+    await res.json({
+      errorMessage: resp.body.rm,
+    });
+    return;
+  }
+  await res.status(resp.statusCode);
+  await res.json({ lottery: resp.body.results.coupon.object_info.content });
 };
