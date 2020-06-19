193552a43c7f674b65cbced2423241cb5bda9290
aiyu666 <zrobin8741@gmail.com>
2020-06-20 01:19:51 +0800

    Add today lottery

--- controllers/lotteryController.js
+++ controllers/lotteryController.js
@@ -1,17 +1,35 @@
 const lottery = require('../models/lottery');
 
 async function getLottery(req, res) {
-  const resp = await lottery.getLottery(req.body.accessToken);
-  await console.log(resp.body);
-  if (resp.body.rc !== 1) {
-    await res.status(resp.statusCode);
+  const nowDate = new Date();
+  nowDate.setDate(nowDate.getDate() + 2);
+  console.log(nowDate.format('yyyy/mm/dd 00:00:00'));
+  const getLotteryResp = await lottery.getLottery(req.body.accessToken);
+  const getLotteryListResp = await lottery.getLotteryList(req.body.accessToken);
+  const stickerListResp = await lottery.getStickerList(req.body.accessToken);
+  await console.log(getLotteryListResp.body.results.coupons[0].object_info.redeem_end_datetime);
+  const lotteryToday = await getLotteryListResp.body.results.coupons.filter(
+    (lotteryTarget) => lotteryTarget.object_info.redeem_end_datetime === nowDate.format('yyyy/mm/dd 23:59:59'),
+  );
+  const stickerToday = await stickerListResp.body.results.stickers.filter(
+    (stickerTarget) => stickerTarget.obtain_datetime > new Date().format('yyyy/mm/dd 00:00:00'),
+  );
+  await console.log(lotteryToday);
+  await console.log(stickerToday);
+  const todayGet = (lotteryToday.length !== 0) ? await lotteryToday[0].object_info.title : '歡樂貼QQ';
+  if (getLotteryResp.body.rc !== 1) {
+    await res.status(getLotteryResp.statusCode);
     await res.json({
-      errorMessage: resp.body.rm,
+      errorMessage: getLotteryResp.body.rm,
     });
     return;
   }
-  await res.status(resp.statusCode);
-  await res.json({ lottery: resp.body.results.coupon.object_info.title });
+
+  await res.status(getLotteryResp.statusCode);
+  await res.json({
+    lottery: getLotteryResp.body.results.coupon.object_info.title,
+    todayLottery: todayGet,
+  });
 }
 
 async function getLotteryStatus(req, res) {
