9e190809850c436997265a396f82c6384bb94d44
aiyu666 <zrobin8741@gmail.com>
2020-06-26 17:21:26 +0800

    Remove no need request method

--- models/request.js
+++ models/request.js
@@ -3,18 +3,6 @@ const request = require('request');
 /**
  * @param  {object} options the object contents request url, body ,headers
  */
-async function getRequest(options) {
-  return new Promise((resolve, reject) => {
-    request.get(options, (error, response) => {
-      if (error) reject(new Error(`Ops! Some error about get request-> ${error}`));
-      resolve(response);
-    });
-  });
-}
-
-/**
- * @param  {object} options the object contents request url, body ,headers
- */
 async function postRequest(options) {
   return new Promise((resolve, reject) => {
     request.post(options, (error, response) => {
@@ -25,6 +13,5 @@ async function postRequest(options) {
 }
 
 module.exports = {
-  getRequest,
   postRequest,
 };
