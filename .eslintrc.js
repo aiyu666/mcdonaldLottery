e803ee0987417b2a81fb26cfdcf6ac088380dde5
aiyu666 <zrobin8741@gmail.com>
2020-06-26 17:22:20 +0800

    Add jest in project

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,8 +2,10 @@ module.exports = {
     "env": {
         "commonjs": true,
         "es6": true,
-        "node": true
+        "node": true,
+        "jest/globals": true
     },
+    "plugins": ["jest"],
     "extends": [
         "airbnb-base"
     ],
@@ -15,5 +17,10 @@ module.exports = {
         "ecmaVersion": 2018
     },
     "rules": {
+        "jest/no-disabled-tests": "warn",
+        "jest/no-focused-tests": "error",
+        "jest/no-identical-title": "error",
+        "jest/prefer-to-have-length": "warn",
+        "jest/valid-expect": "error"
     }
 };
