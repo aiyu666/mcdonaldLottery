9b5020f83cd1fd4f74f0625a6b36b46b7e6a859f
aiyu666 <zrobin8741@gmail.com>
2020-06-26 17:22:53 +0800

    Add test for get lottery and token

---
+++ test/lottery.test.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const Ajv = require('ajv');
+const lotterySchema = require('./schema/lottery.json');
+
+const ajv = new Ajv();
+
+const lottery = require('../models/lottery');
+
+test('Can get lottery', async () => {
+  const resp = await lottery.getLottery(process.env.ACCESS_TOKEN);
+  await expect(resp.statusCode).toBe(200);
+  await expect(ajv.validate(lotterySchema.getLottery, resp.body)).toBe(true);
+  await expect(resp.body.rc).toBe(1);
+});
+
+test('Can get lottery list', async () => {
+  const resp = await lottery.getLotteryList(process.env.ACCESS_TOKEN);
+  await expect(resp.statusCode).toBe(200);
+  await expect(ajv.validate(lotterySchema.getLotteryList, resp.body)).toBe(true);
+  await expect(resp.body.rc).toBe(1);
+  await expect(resp.body.rm).toBe('成功');
+});
+
+test('Can get sticker list', async () => {
+  const resp = await lottery.getStickerList(process.env.ACCESS_TOKEN);
+  await expect(resp.statusCode).toBe(200);
+  await expect(ajv.validate(lotterySchema.getStickerList, resp.body)).toBe(true);
+  await expect(resp.body.rc).toBe(1);
+  await expect(resp.body.rm).toBe('成功');
+});
---
+++ test/user.test.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const Ajv = require('ajv');
+const userSchema = require('./schema/user.json');
+
+const ajv = new Ajv();
+
+const user = require('../models/user');
+
+test('Can get user token', async () => {
+  const resp = await user.getToken(process.env.USER_ACCOUNT, process.env.USER_PASSWORD);
+  await expect(resp.statusCode).toBe(200);
+  await expect(ajv.validate(userSchema.getUserToekn, resp.body)).toBe(true);
+  await expect(resp.body.rc).toBe('1');
+  await expect(resp.body.rm).toBe('登入成功');
+});
