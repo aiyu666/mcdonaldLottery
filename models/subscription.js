a673e3519e7dbde3a53a0e3cb5bc19993eaad9d4
aiyu666 <zrobin8741@gmail.com>
2020-07-25 19:00:18 +0800

    Update the send line notify message content

--- models/subscription.js
+++ models/subscription.js
@@ -8,5 +8,5 @@ module.exports = (accessToken) => scheduleJob(process.env.CRON_FORMAT, async () 
   const getLotteryResp = await getLottery(accessToken);
   if (getLotteryResp.statusCode !== 200) return sendLineNotify(`領取失敗,因為 statusCode 拿到 ${getLotteryResp.statusCode}`);
   if (getLotteryResp.body.rc !== 1) return sendLineNotify('領取失敗');
-  return sendLineNotify(getLotteryResp.body.results.coupon.object_info.title);
+  return sendLineNotify(`\n${getLotteryResp.body.results.coupon.object_info.title}`);
 });
