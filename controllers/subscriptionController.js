ebdb8d97044f8eac84821a23ddb3b04384776256
aiyu666 <zrobin8741@gmail.com>
2020-07-26 13:29:00 +0800

    Add cronFromat in controller

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -1,7 +1,8 @@
 const subscription = require('../models/subscription.js');
 
 module.exports = async (req, res) => {
-  const resp = await subscription(req.body.accessToken);
+  const requestBody = JSON.parse(req.body);
+  const resp = await subscription(requestBody.accessToken, requestBody.cronFromat);
   res.status(200);
   await res.json({
     name: resp.name,
