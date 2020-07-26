defdf9df605025ab4656b0972d6e360ad1cfdb86
aiyu666 <zrobin8741@gmail.com>
2020-07-26 13:26:51 +0800

    Add option for subscription about cronFormat

--- controllers/subscriptionController.js
+++ controllers/subscriptionController.js
@@ -1,9 +1,10 @@
 const subscription = require('../models/subscription.js');
 
 module.exports = async (req, res) => {
-  subscription(req.body.accessToken);
+  const resp = await subscription(req.body.accessToken);
   res.status(200);
   await res.json({
+    name: resp.name,
     message: 'success',
   });
 };
--- models/subscription.js
+++ models/subscription.js
@@ -4,9 +4,12 @@ const { getLottery } = require('./lottery');
 require('dotenv').config();
 
 
-module.exports = (accessToken) => scheduleJob(process.env.CRON_FORMAT, async () => {
-  const getLotteryResp = await getLottery(accessToken);
-  if (getLotteryResp.statusCode !== 200) return sendLineNotify(`領取失敗,因為 statusCode 拿到 ${getLotteryResp.statusCode}`);
-  if (getLotteryResp.body.rc !== 1) return sendLineNotify('領取失敗');
-  return sendLineNotify(`\n${getLotteryResp.body.results.coupon.object_info.title}`);
-});
+module.exports = (accessToken, cronFromat = process.env.CRON_FORMAT) => {
+  const resp = scheduleJob(cronFromat, async () => {
+    const getLotteryResp = await getLottery(accessToken);
+    if (getLotteryResp.statusCode !== 200) return sendLineNotify(`領取失敗,因為 statusCode 拿到 ${getLotteryResp.statusCode}`);
+    if (getLotteryResp.body.rc !== 1) return sendLineNotify('領取失敗');
+    return sendLineNotify(`\n${getLotteryResp.body.results.coupon.object_info.title}`);
+  });
+  return resp;
+};
