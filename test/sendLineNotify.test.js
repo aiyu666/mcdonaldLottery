5b96d17de2c4fe836269271d8e579cff2e927523
aiyu666 <zrobin8741@gmail.com>
2020-08-22 17:04:59 +0800

    Add verify schema in line notify test

--- test/sendLineNotify.test.js
+++ test/sendLineNotify.test.js
@@ -1,15 +1,15 @@
 require('dotenv').config();
-// const Ajv = require('ajv');
-// const sendLineNotifySchema = require('./schema/sendLineNotify.json');
+const Ajv = require('ajv');
+const sendLineNotifySchema = require('./schema/sendLineNotify.json');
+const sendLineNotify = require('../module/sendLineNotify');
 
-// const ajv = new Ajv();
-
-const sendLineNotify = require('../models/sendLineNotify');
+const ajv = new Ajv();
 
 describe('Send line notify integration test', () => {
   test('Can send line notify', async () => {
     const resp = await sendLineNotify('test', process.env.LINE_NOTIFY_TOKEN);
     await expect(resp.statusCode).toBe(200);
+    await expect(ajv.validate(sendLineNotifySchema, resp.body)).toBe(true);
     await expect(resp.body.status).toBe(200);
     await expect(resp.body.message).toBe('ok');
   });
